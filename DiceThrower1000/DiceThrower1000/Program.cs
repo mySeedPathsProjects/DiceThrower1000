43f87760aff0cf9aa162063f6d7803bf853768bb
Nicole Fleit <nicole.fleitDenver@gmail.com>
2015-02-03 16:20:44 -0700

    Made The Thrower1000

---
+++ DiceThrower1000/DiceThrower1000/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceThrower1000
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            ThrowDice("10d6");
+            ThrowDice("3d20");
+            ThrowDice("100d6");
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// See the results of dice
+        /// </summary>
+        /// <param name="diceString"></param>
+        static void ThrowDice(string diceString)
+        {
+            Random rng = new Random();
+
+            string resultString = "";
+            string[] inputArray = diceString.Split('d');
+
+            int numberOfThrows = int.Parse(inputArray[0]);
+            int numberOfSides = int.Parse(inputArray[1]);
+
+            for (int i = 0; i < numberOfThrows; i++)
+            {
+                int rollResult = rng.Next(1, numberOfSides + 1);
+                resultString += (rollResult.ToString() + " ");
+            }
+            Console.WriteLine("Throwing: " + diceString);
+            Console.WriteLine("Results: " + resultString);
+        }
+    }
+}
